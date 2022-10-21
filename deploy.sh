488ebfda1d71392d6532509f4771cac291e84e13
kunkun.lu <kunkun.lu@ly.com>
2022-10-21 21:48:54 +0800

    修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 构建
-npm run docs:build
+npm run build
 
 # 进入待发布的目录
 cd docs/.vitepress/dist
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:willianLu/68.git master:gh-pages
 
 cd -
